8a98cb42f6d0edf6598b3ebd8276c048bfaeace3
余承軒 <cheng-syuanyu@pc159.wireless12.tku.edu.tw>
2017-05-18 18:25:13 +0800

    take

--- Messenger/AppDelegate.swift
+++ Messenger/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        let layout = UICollectionViewFlowLayout()
+        let friendsController = FriendsController(collectionViewLayout:layout)
+        window?.rootViewController = UINavigationController(rootViewController:friendsController)
+        
         // Override point for customization after application launch.
         return true
     }
@@ -41,11 +48,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
+        if #available(iOS 10.0, *) {
+            self.saveContext()
+        } else {
+            // Fallback on earlier versions
+        }
     }
 
     // MARK: - Core Data stack
 
+    @available(iOS 10.0, *)
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
@@ -75,6 +87,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data Saving support
 
+    @available(iOS 10.0, *)
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsController: UICollectionViewController{
 
+    private let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(FriendsCell.self, forCellWithReuseIdentifier: cellId)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellId = collectionView.dequeueReusableCell(withReuseIdentifier: "CellIdentifier", for: indexPath as IndexPath) as UICollectionViewCell
+        
+        return cellId
+    }
+ 
 
 
+}
+class FriendsCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        setupViews()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setupViews(){
+        backgroundColor = UIColor.gray
+    }
 }
 
