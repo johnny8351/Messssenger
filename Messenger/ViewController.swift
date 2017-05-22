4d2120d8c5a35e1a018db7e347408be4b9085884
余承軒 <cheng-syuanyu@cheng-syuanyudemacbook-air.local>
2017-05-22 11:46:16 +0800

    build layout

--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -49,38 +49,99 @@ class FriendsCell: BaseCell {
         view.backgroundColor = UIColor(white:0.5,alpha:0.5)
         return view
     }()
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Trunks"
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your friend's message & something else..."
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = UIColor.black
+        return label
+    }()
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1:15pm"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .right
+        return label
+    }()
+    let hasReadImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
     override func setupViews(){
         
         addSubview(profileImageView)
         addSubview(dividerLineView)
+        setupContainerView()
         profileImageView.image = UIImage(named:"baby")
+        hasReadImageView.image = UIImage(named:"baby")
+        /*
         profileImageView.translatesAutoresizingMaskIntoConstraints = false
         dividerLineView.translatesAutoresizingMaskIntoConstraints = false
-        /*
+        
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-12-[v0(68)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":profileImageView]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-12-[v0(68)]", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":profileImageView]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-82-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":dividerLineView]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[v0(1)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":dividerLineView]))
         //加入約束
-        */
-        addConstraintsWithFormat(format: "H:|-12-[v0(68)]|", views: profileImageView)
-        addConstraintsWithFormat(format: "V:|-12-[v0(68)]", views: profileImageView)
+         */
+        
+        addConstraintsWithFormat(format: "H:|-12-[v0(68)]", views: profileImageView)
+        addConstraintsWithFormat(format: "V:[v0(68)]", views: profileImageView)
+        addConstraints([NSLayoutConstraint(item: profileImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)])
         addConstraintsWithFormat(format: "H:|-82-[v0]|", views: dividerLineView)
         addConstraintsWithFormat(format: "V:[v0(1)]|", views: dividerLineView)
         
     }
+    private func setupContainerView(){
+        let containerView = UIView()
+        addSubview(containerView)
+        
+        addConstraintsWithFormat(format: "H:|-90-[v0]|", views: containerView)
+        addConstraintsWithFormat(format: "V:[v0(50)]", views: containerView)
+        addConstraints([NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)])
+        
+        containerView.addSubview(nameLabel) //在containerView裡的
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(hasReadImageView)
+        
+        addConstraintsWithFormat(format: "H:|[v0][v1(80)]-12-|", views: nameLabel,timeLabel)
+        
+        addConstraintsWithFormat(format: "V:|[v0][v1(24)]|", views: nameLabel,messageLabel)//共用高
+        
+        addConstraintsWithFormat(format: "H:|[v0]-8-[v1(20)]-12-|", views: messageLabel,hasReadImageView)
+        
+        addConstraintsWithFormat(format: "V:|[v0(24)]", views: timeLabel)
+        
+        addConstraintsWithFormat(format: "V:[v0(20)]|", views: hasReadImageView)
+        
+        
+       
+        //addConstraints(<#T##constraints: [NSLayoutConstraint]##[NSLayoutConstraint]#>)
+    }
 }
+
 extension UIView{
     func addConstraintsWithFormat(format:String,views:UIView...){
-        var viewDictionary = [String:UIView]()
+        var viewsDictionary = [String:UIView]()
         for(index,view) in views.enumerated(){
             let key = "v\(index)"
-            viewDictionary[key] = view
+            viewsDictionary[key] = view
             view.translatesAutoresizingMaskIntoConstraints = false
         }
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat:format, options: NSLayoutFormatOptions(), metrics: nil, views: viewDictionary))
-        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat:format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
 }
+
 class BaseCell: UICollectionViewCell{
     override init(frame: CGRect) {
         super.init(frame:frame)
